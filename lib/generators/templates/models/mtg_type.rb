ee5cb303455856692c5e0840d612bff28dad15c8
Joel Andritsch <joel.andritsch@gettyimages.com>
2012-11-29 17:58:27 -0600

    Fix class name

--- lib/generators/templates/models/mtg_type.rb
+++ lib/generators/templates/models/mtg_type.rb
@@ -1,4 +1,4 @@
-class MtgCardType < ActiveRecord::Base
+class MtgType < ActiveRecord::Base
   has_many :mtg_cards, :through => "mtg_cards_mtg_types"
   validates :name, :presence => true, :uniqueness => true
 end
