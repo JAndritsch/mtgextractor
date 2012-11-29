e830b7039068fb095d0a9d2da8d20fa9e4f8526c
Joel Andritsch <joel.andritsch@gettyimages.com>
2012-11-29 14:36:40 -0600

    Reorder generator file creation

--- lib/generators/mtgextractor_generator.rb
+++ lib/generators/mtgextractor_generator.rb
@@ -16,16 +16,17 @@ module MTGExtractor
       @prev_migration_nr.to_s
     end
 
+    def copy_card_classes
+      copy_file "models/mtg_card.rb", "app/models/mtg_card.rb"
+      copy_file "models/mtg_set.rb", "app/models/mtg_set.rb"
+    end
+
     def create_migration_files
       migration_template "migrations/create_mtg_cards.rb", "db/migrate/create_mtg_cards.rb"
       migration_template "migrations/create_mtg_sets.rb", "db/migrate/create_mtg_sets.rb"
       migration_template "migrations/create_mtg_cards_mtg_sets.rb", "db/migrate/create_mtg_cards_mtg_sets.rb"
     end
 
-    def copy_card_classes
-      copy_file "models/mtg_card.rb", "app/models/mtg_card.rb"
-      copy_file "models/mtg_set.rb", "app/models/mtg_set.rb"
-    end
 
   end
 end
