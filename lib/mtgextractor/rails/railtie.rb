657c00486d10b52cd5db5935e099c6466d06e68c
Joel Andritsch <joel.andritsch@gettyimages.com>
2012-11-29 08:39:02 -0600

    Remove generator require in railtie

--- lib/mtgextractor/rails/railtie.rb
+++ lib/mtgextractor/rails/railtie.rb
@@ -5,9 +5,5 @@ module MTGExtractor
     rake_tasks do
       Dir[File.join(File.dirname(__FILE__), 'tasks/*.rake')].each { |f| load f }
     end
-
-    generators do
-      require 'generators/mtgextractor_generator.rb'
-    end
   end
 end
